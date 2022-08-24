662ad9dfa88fb05c2773bdcf7aa5b2f406e8be12
aminul007bd <gem007be@gmail.com>
2022-08-24 12:14:17 -0700

    ext demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'man-ext-text-01';
+  firstMediaItem = {
+    id: 1,
+    name: 'Firebug',
+    medium: 'Series',
+    category: 'Science Fiction',
+    year: 2010,
+    watchedOn: 12312312312312,
+    isFavorite: false,
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component';
+import { MatCardModule } from '@angular/material/card';
+import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
+const lookupLists = {
+  mediums: ['Movies', 'Series'],
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HeroesComponent,
+    ContactFormComponent,
+    MediaItemListComponent,
+    FavoriteDirective,
+    CategoryListPipe,
+    MediaItemFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: 'lookupListToken', useValue: lookupLists }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
